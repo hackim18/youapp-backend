cef8a6574f45c66c3d12db8141483e533b228a7d
Hackim18 <khakim668@gmail.com>
2025-11-25 17:09:24 +0700

    feat: add new messaging constant, module, and service

--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -1,8 +1,28 @@
-﻿import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+import configuration from '../config/configuration';
+import { RABBITMQ_CLIENT } from './messaging.constants';
 import { MessagingService } from './messaging.service';
 
 @Module({
-  providers: [MessagingService],
-  exports: [MessagingService],
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: RABBITMQ_CLIENT,
+      inject: [configuration.KEY],
+      useFactory: (config: ConfigType<typeof configuration>) =>
+        ClientProxyFactory.create({
+          transport: Transport.RMQ,
+          options: {
+            urls: [config.rabbitmq.url],
+            queue: config.rabbitmq.queue,
+            queueOptions: { durable: true },
+          },
+        }),
+    },
+    MessagingService,
+  ],
+  exports: [MessagingService, RABBITMQ_CLIENT],
 })
 export class MessagingModule {}
--- src/messaging/messaging.service.ts
+++ src/messaging/messaging.service.ts
@@ -1,4 +1,15 @@
-﻿import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { RABBITMQ_CLIENT } from './messaging.constants';
 
 @Injectable()
-export class MessagingService {}
+export class MessagingService {
+  constructor(
+    @Inject(RABBITMQ_CLIENT)
+    private readonly client: ClientProxy,
+  ) {}
+
+  emit(pattern: string, payload: unknown) {
+    return this.client.emit(pattern, payload);
+  }
+}
