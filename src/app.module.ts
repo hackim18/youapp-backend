56b82d7af4955dc7a8531bba8e8c55201864adf1
Hackim18 <khakim668@gmail.com>
2025-11-25 16:25:41 +0700

    feat: add empty business logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+﻿import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { MessagingModule } from './messaging/messaging.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ProfilesModule } from './modules/profiles/profiles.module';
+import { ChatModule } from './modules/chat/chat.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    MessagingModule,
+    AuthModule,
+    UsersModule,
+    ProfilesModule,
+    ChatModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
