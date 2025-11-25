589cab312a7db93a25741d38431817732f3d06c9
Hackim18 <khakim668@gmail.com>
2025-11-25 16:53:37 +0700

    feat: add new module for database mongodb

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,19 @@
-﻿import { Module } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from '../config/configuration';
 
 @Module({
-  providers: [DatabaseService],
-  exports: [DatabaseService],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [configuration.KEY],
+      useFactory: (config: ConfigType<typeof configuration>) => ({
+        uri: config.mongo.uri,
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
 })
 export class DatabaseModule {}
